69d32c88e1a1bd2d333beb99120eb64d3b65bd7a
Nitin <er.nitinpatel914@gmail.com>
2018-04-03 00:47:15 -0500

    registering user page added

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers: [UserService]
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  userRegister: FormGroup;
+  loading: boolean;
+
+  constructor(private fb: FormBuilder, private router: Router, 
+    private userService: UserService) { 
+      this.userRegister = this.fb.group({
+        username: ['', Validators.required],
+        email: ['',[ Validators.required, Validators.email ]],
+        password: ['',[ Validators.required, Validators.minLength(6) ]]
+      });
+    }
 
   ngOnInit() {
+    this.loading = false;
+  }
+
+  onRegister(){
+    this.loading = true;
+
   }
 
 }
